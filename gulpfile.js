2a95f37b0e32349de6fb287414a400403f1a3410
drmyersii <drmyersii@users.noreply.github.com>
2015-05-15 16:01:09 -0400

    Adding some default gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,37 @@
 var gulp = require('gulp');
 
-gulp.task('default', function () {
-    
+
+var paths = {
+    scripts: ['app/coffee/main.coffee'],
+    styles: ['app/less/main.less']
+};
+
+
+gulp.task('scripts-compile', function () {
+
 });
+
+gulp.task('scripts-minify', function () {
+
+});
+
+
+gulp.task('styles-compile', function () {
+
+});
+
+gulp.task('styles-minify', function () {
+
+});
+
+
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['scripts-compile']);
+    gulp.watch(paths.styles, ['styles-compile']);
+});
+
+gulp.task('prod', function () {
+
+});
+
+gulp.task('default', ['prod']);
